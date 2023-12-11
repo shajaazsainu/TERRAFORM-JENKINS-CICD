e66618f2e253969f001943ca412b91aec48559f6
shajaazsainu <86217249+shajaazsainu@users.noreply.github.com>
2023-12-11 12:34:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "project-cicd-7777"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "project-dynamo-db-table"
   }
 }
